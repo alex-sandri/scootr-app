0ac16df774047cafb51fd7ae19abb1ae66d08d37
Alex Sandri <me@alexsandri.com>
2021-04-29 16:27:35 +0200

    Install 'WorkSans' font family

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "scootr",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        fontFamily: "WorkSans",
       ),
       home: HomeRoute(),
     );
