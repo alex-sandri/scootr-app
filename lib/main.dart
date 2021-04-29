aee9799d6a0627c23e10b06b7052363ced772e3d
Alex Sandri <me@alexsandri.com>
2021-04-29 17:20:35 +0200

    Add assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:scootr/config/Config.dart';
 import 'package:scootr/routes/Home.dart';
 
 void main() {
@@ -8,10 +10,38 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
+    );
+
     return MaterialApp(
       title: "scootr",
       theme: ThemeData(
         fontFamily: "WorkSans",
+        scaffoldBackgroundColor: Config.PRIMARY_COLOR,
+        appBarTheme: AppBarTheme(
+          brightness: Brightness.dark,
+          color: Config.PRIMARY_COLOR,
+          elevation: 0,
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all(Config.PRIMARY_COLOR),
+            backgroundColor: MaterialStateProperty.all(Config.SECONDARY_COLOR),
+            padding: MaterialStateProperty.all(EdgeInsets.all(10)),
+            textStyle: MaterialStateProperty.all(TextStyle(
+              fontSize: 18,
+            )),
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            )),
+          ),
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Config.SECONDARY_COLOR,
+        ),
       ),
       home: HomeRoute(),
     );
--- lib/routes/Home.dart
+++ lib/routes/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:scootr/config/Config.dart';
 import 'package:scootr/widgets/AppBar.dart';
 
 class HomeRoute extends StatelessWidget {
@@ -6,6 +8,44 @@ class HomeRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: ScootrAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextButton.icon(
+              icon: SvgPicture.asset(
+                "assets/images/spid-icon.svg",
+                height: 25,
+              ),
+              label: Text("Entra con SPID"),
+              style: Theme.of(context).textButtonTheme.style.copyWith(
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+                backgroundColor: MaterialStateProperty.all(Config.SPID_COLOR),
+              ),
+              onPressed: () {
+
+              },
+            ),
+            SizedBox(height: 10),
+            TextButton.icon(
+              icon: SvgPicture.asset(
+                "assets/images/cie-icon.svg",
+                height: 25,
+              ),
+              label: Text("Entra con CIE"),
+              style: Theme.of(context).textButtonTheme.style.copyWith(
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+                backgroundColor: MaterialStateProperty.all(Config.SPID_COLOR),
+              ),
+              onPressed: () {
+
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
