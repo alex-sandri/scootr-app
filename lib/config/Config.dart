9639aef1b6ea0590ea2da67c4b8788fb47c43e47
Alex Sandri <me@alexsandri.com>
2021-04-30 20:21:09 +0200

    Update PRIMARY_COLOR in Config

--- lib/config/Config.dart
+++ lib/config/Config.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:scootr/models/SpidIdentityProvider.dart';
 
 class Config {
-  static const Color PRIMARY_COLOR = Colors.green;
+  static const Color PRIMARY_COLOR = Color(0xFF008000);
   static const Color SECONDARY_COLOR = Colors.white;
 
   static const Color SPID_COLOR = Color(0xff0066cc);
