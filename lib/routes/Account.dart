949fa8cd6b465473eb88382924f75abfe2eefb9b
Alex Sandri <me@alexsandri.com>
2021-05-02 18:44:18 +0200

    Add more tabs

--- lib/routes/Account.dart
+++ lib/routes/Account.dart
@@ -6,17 +6,30 @@ class AccountRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 1,
+      length: 3,
       child: Scaffold(
         appBar: ScootrAppBar(
           title: "Account",
           tabs: [
-            Tab(icon: Icon(Icons.account_circle), text: "Dettagli"),
+            Tab(
+              icon: Icon(Icons.account_circle),
+              text: "Dettagli",
+            ),
+            Tab(
+              icon: Icon(Icons.account_balance_wallet),
+              text: "Portafogli",
+            ),
+            Tab(
+              icon: Icon(Icons.bike_scooter),
+              text: "Corse",
+            ),
           ],
         ),
         body: TabBarView(
           children: [
-            Text("asa"),
+            Text("Dettagli"),
+            Text("Portafogli"),
+            Text("Corse"),
           ],
         ),
         drawer: ScootrDrawer(),
