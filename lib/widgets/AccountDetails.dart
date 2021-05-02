f939f5ca8b248fe068893fb3e63d45993089960d
Alex Sandri <me@alexsandri.com>
2021-05-02 18:59:13 +0200

    Show user details

--- lib/widgets/AccountDetails.dart
+++ lib/widgets/AccountDetails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:scootr/models/Session.dart';
 import 'package:scootr/services/Auth.dart';
@@ -13,6 +14,22 @@ class AccountDetails extends StatelessWidget {
         ListTile(
           title: Text("Nome"),
           subtitle: Text(session.user.firstName),
+        ),
+        ListTile(
+          title: Text("Cognome"),
+          subtitle: Text(session.user.lastName),
+        ),
+        ListTile(
+          title: Text("Email"),
+          subtitle: Text(session.user.email),
+        ),
+        ListTile(
+          title: Text("Data di nascita"),
+          subtitle: Text(DateFormat.yMMMMd(Localizations.localeOf(context).toString()).format(session.user.birthDate)),
+        ),
+        ListTile(
+          title: Text("Codice Fiscale"),
+          subtitle: Text(session.user.fiscalNumber),
         ),
       ],
     );
