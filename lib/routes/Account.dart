910e37891b604ef08c8c9c8a2b7f441214b4c312
Alex Sandri <me@alexsandri.com>
2021-05-02 18:21:25 +0200

    Create Drawer widget

--- lib/routes/Account.dart
+++ lib/routes/Account.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
-import 'package:scootr/models/Session.dart';
-import 'package:scootr/services/Auth.dart';
 import 'package:scootr/widgets/AppBar.dart';
+import 'package:scootr/widgets/Drawer.dart';
 
 class AccountRoute extends StatelessWidget {
   @override
@@ -13,47 +10,7 @@ class AccountRoute extends StatelessWidget {
         title: "Account",
       ),
       body: Container(),
-      drawer: Builder(
-        builder: (context) {
-          final AuthService auth = Provider.of<AuthService>(context, listen: false);
-          final Session session = auth.session!;
-
-          return Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: [
-                DrawerHeader(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "${session.user.firstName} ${session.user.lastName}",
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      Text(session.user.email),
-                      Text(DateFormat.yMMMMd(Localizations.localeOf(context).toString()).format(session.user.birthDate)),
-                      Text(session.user.fiscalNumber),
-                    ],
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.account_circle),
-                  title: Text("Account"),
-                  onTap: () {
-                    
-                  },
-                ),
-                Divider(),
-                ListTile(
-                  leading: Icon(Icons.logout),
-                  title: Text("Esci"),
-                  onTap: auth.signOut,
-                ),
-              ],
-            ),
-          );
-        }
-      ),
+      drawer: ScootrDrawer(),
     );
   }
 }
---
+++ lib/widgets/Drawer.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:scootr/models/Session.dart';
+import 'package:scootr/routes/Account.dart';
+import 'package:scootr/services/Auth.dart';
+
+class ScootrDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final AuthService auth = Provider.of<AuthService>(context, listen: false);
+    final Session session = auth.session!;
+
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "${session.user.firstName} ${session.user.lastName}",
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                Text(session.user.email),
+                Text(DateFormat.yMMMMd(Localizations.localeOf(context).toString()).format(session.user.birthDate)),
+                Text(session.user.fiscalNumber),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.account_circle),
+            title: Text("Account"),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => AccountRoute(),
+                ),
+              );
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text("Esci"),
+            onTap: auth.signOut,
+          ),
+        ],
+      ),
+    );
+  }
+}
