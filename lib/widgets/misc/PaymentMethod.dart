53ed7a3290ad0d4e4fa6f1781ba860e9cdcb036a
Alex Sandri <me@alexsandri.com>
2021-05-05 18:16:43 +0200

    Show payment method info for card type

--- lib/widgets/misc/PaymentMethod.dart
+++ lib/widgets/misc/PaymentMethod.dart
@@ -1,22 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:scootr/config/Config.dart';
 import 'package:scootr/models/PaymentMethod.dart';
-import 'package:scootr/models/Wallet.dart';
-import 'package:scootr/services/Api.dart';
+
+enum PaymentMethodButton
+{
+  SET_DEFAULT,
+  DELETE,
+}
 
 class PaymentMethodWidget extends StatelessWidget {
   final PaymentMethod _paymentMethod;
 
   PaymentMethodWidget(this._paymentMethod);
 
+  Widget? _getTitle() {
+    switch (_paymentMethod.type)
+    {
+      case "card":
+      {
+        return Text(
+          (_paymentMethod.data["brand"] as String).toUpperCase(),
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+          ),
+        );
+      }
+    }
+  }
+
+  Widget? _getSubtitle() {
+    switch (_paymentMethod.type)
+    {
+      case "card":
+      {
+        return Text(
+          "${(_paymentMethod.data["exp_month"] as int).toRadixString(10).padLeft(2, "0")} / ${_paymentMethod.data["exp_year"]}"
+          "    "
+          "\u2022\u2022\u2022\u2022 ${_paymentMethod.data["last4"]}",
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [
-          ListTile(
-            leading: Text(_paymentMethod.data["brand"]),
-          ),
-        ],
+      child: ListTile(
+        title: _getTitle(),
+        subtitle: _getSubtitle(),
+        trailing: PopupMenuButton(
+          itemBuilder: (context) {
+            return [
+              const PopupMenuItem<PaymentMethodButton>(
+                value: PaymentMethodButton.SET_DEFAULT,
+                child: Text("Imposta come predefinito"),
+              ),
+              const PopupMenuItem<PaymentMethodButton>(
+                value: PaymentMethodButton.DELETE,
+                child: Text(
+                  "Elimina",
+                  style: TextStyle(
+                    color: Config.DANGER_COLOR,
+                  ),
+                ),
+              ),
+            ];
+          },
+        ),
       ),
     );
   }
